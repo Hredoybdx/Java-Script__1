f6c50f538229d16ab6b90a4087a2d79f039acee5
JEWEL AHMMED <jewelbdx@gmail.com>
2016-08-25 23:51:11 +0600

    Day 01 Added

---
+++ Day-01/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>JavaScript</title>
+        <style>
+            #demo {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+
+        <div id="demo"></div>
+
+        <script>
+            document.getElementById("demo").innerHTML = "Hi, I am JavaScript";
+        </script>
+
+    </body>
+</html>
