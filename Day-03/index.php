4a11a6f29b3eabf9203ae2eeac51bdb5f4460f63
JEWEL AHMMED <jewelbdx@gmail.com>
2016-08-29 02:39:23 +0600

    Day 03 Added

---
+++ Day-03/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="script.js"></script>
+    </head>
+
+    <body>
+
+        <h1>My Web Page</h1>
+
+        <p id="demo">A Paragraph</p>
+
+        <button type="button" onclick="xyz()">Delete Text</button>
+
+        <script>
+            // window.alert(5 * 5);
+            /*
+             window.alert("5 + 5");
+
+             document.write('Hello JS');
+
+             */
+            console.log('5 + 25'); // String
+            console.log(5 + 25); // Number
+
+            var FirstNumber = true;
+            var second_number = 85;
+
+
+
+            second_number = 200;
+
+            var MyVar;
+
+            MyVar = second_number;
+
+            console.log(typeof MyVar);
+
+
+
+
+
+
+
+
+
+
+        </script>
+
+        <button onclick="document.write(5 + 6)">Try it</button>
+
+    </body>
+</html>
---
+++ Day-03/script.js
@@ -0,0 +1,3 @@
+function xyz() {
+    document.getElementById("demo").innerHTML = "I am JS";
+}
