7eb264854aa573c9b00da1660c2981977921e02c
JEWEL AHMMED <jewelbdx@gmail.com>
2016-08-30 21:22:31 +0600

    Day 4 Added

---
+++ Day-04/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="script.js"></script>
+    </head>
+
+    <body>
+
+        <h1>My Web Page</h1>
+
+        <p id="demo">A Paragraph</p>
+
+        <button type="button" onclick="xyz()">Delete Text</button>
+
+        <script>
+            // window.alert(5 * 5);
+
+
+
+            /*
+             window.alert("5 + 5");
+
+             document.write('Hello JS');
+
+             */
+            console.log('5 + 25'); // String
+            console.log(5 + 25); // Number
+
+            var FirstNumber = true;
+            var second_number = 85;
+
+            second_number = 200;
+
+            var MyVar;
+
+            MyVar = second_number;
+
+            console.log(typeof MyVar);
+
+
+            var Num = (6 + 5) * 3 * (1 + 5);
+
+            var person = "Hege";
+            var person = "Hege";
+
+            document.getElementById("demo").innerHTML =
+                    "Hello Dolly.";
+
+
+            var functionx = 10;
+
+
+
+
+
+
+        </script>
+
+        <button onclick="document.write(5 + 6)">Try it</button>
+
+    </body>
+</html>
---
+++ Day-04/script.js
@@ -0,0 +1,20 @@
+/*
+ This is a xyz function.
+ */
+
+function xyz() {
+    document.getElementById("demo").innerHTML = "I am JS";
+}
+
+
+/**
+ *
+ * @returns {Number} This function will give you a number.
+ */
+
+function my_name() {
+    document.getElementById("demo").innerHTML = "Hello Dolly.";
+    document.getElementById("myDIV").innerHTML = "How are you?";
+
+    return  10;
+}
