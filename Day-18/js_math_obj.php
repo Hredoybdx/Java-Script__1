43aa0877192686bca2a545157384bb2247108335
JEWEL AHMMED <jewelbdx@gmail.com>
2016-10-17 00:26:38 +0600

    Day 18 Added

---
+++ Day-18/js_math_obj.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>JS</title>
+    </head>
+    <body>
+        <script>
+
+            var first_num = prompt('First Number ?');
+            var second_num = prompt('Second Number ?');
+
+            first_num = parseInt(first_num);
+            second_num = parseInt(second_num);
+
+            document.write(first_num + second_num);
+
+
+
+        </script>
+
+
+
+
+
+    </body>
+</html>
+
